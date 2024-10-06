85a5ee9b546a75c5ad00a50a79d480d2bcafae36
Veronica <dv.veronica2075@gmail.com>
2024-10-05 22:54:49 -0300

    genera inputs y mascara

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('inputCard')
+const input = document.querySelector('inputDate')
+const input = document.querySelector('inputCvv')
+
+const maskNumber = "****-****-****-****"
+const maskDate = "**/**"
+const maskCvv = "***"
+
+
+
